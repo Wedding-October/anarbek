a60d91f80274c6b4f3144ec8dc50a5815e0d0398
strannosti <108225468+strannosti@users.noreply.github.com>
2025-09-01 17:14:43 +0600

    Update data.js

--- src/shared/component/Carousel/data.js
+++ src/shared/component/Carousel/data.js
@@ -1,9 +1,9 @@
 
 const cards = [
-    { id: 1, title: "", description: "Описание 1", image: "/src/assets/image/wed1.jpg" },
-    { id: 2, title: "", description: "Описание 2", image: "/src/assets/image/wed2.jpg" },
-    { id: 3, title: "", description: "Описание 3", image: "/src/assets/image/wed3.jpg" },
-    { id: 3, title: "", description: "Описание 3", image: "/src/assets/image/wed4.jpg" },
+    { id: 1, title: "", description: "Описание 1", image: "/anarbek/src/assets/image/wed1.jpg" },
+    { id: 2, title: "", description: "Описание 2", image: "/anarbek/src/assets/image/wed2.jpg" },
+    { id: 3, title: "", description: "Описание 3", image: "/anarbek/src/assets/image/wed3.jpg" },
+    { id: 3, title: "", description: "Описание 3", image: "/anarbek/src/assets/image/wed4.jpg" },
   ];
 
   export default cards
