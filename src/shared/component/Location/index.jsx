031199ee621e2bae58fb497c83bc34d31b305081
AnarbekElina <safdfsdddsfgdfld@gmail.com>
2025-09-18 15:10:55 +0600

    maplatest

--- src/shared/component/Location/index.jsx
+++ src/shared/component/Location/index.jsx
@@ -1,21 +1,26 @@
 import MapWrapper from "./map";
 
+
 import styles from "./location.module.scss";
 import { color } from "framer-motion";
-import { YMaps, Map, Placemark } from "@pbe/react-yandex-maps";
-let i  = 0
+import { YMaps, Map, Placemark , ZoomControl} from "@pbe/react-yandex-maps";
+import { map } from "framer-motion/client";
+import location from "../../../assets/image/location.png"
+// import { ZoomControl } from 'react-leaflet';
+
 const Location = () => {
   return (
-    <div id="location" className={styles.container}>
+    <div id="location" className={styles.container} style={{zIndex:1}}>
+      
       <a  style={{textDecoration: "none"}}href="https://2gis.kg/bishkek/inside/15763234351097281/firm/70000001047984986?m=74.597365%2C42.87277%2F17.86">
           <h3 
            className="text-h3"
-            style={{fontFamily: "AgoniaLyubviRegular", color: "black"}} 
+            style={{fontFamily: "LeJardinFloral", color: "black"}} 
             >Адресс проведения торжества: ул.Токтогулова 125/1
              </h3></a>
       <a href="https://2gis.kg/bishkek/inside/15763234351097281/firm/70000001047984986?m=74.597365%2C42.87277%2F17.86" target="_blank" rel="noopener noreferrer">
       <img 
-        src="/anarbek/src/assets/image/location.png" 
+        src={location}
         alt="" 
         style={{ width: "60px", cursor: "pointer" }} 
       />
@@ -31,27 +36,46 @@ const Location = () => {
       />
 
       <div className={styles.mapTwoGis}>
-        {/* <MapWrapper /> */}
-        <div style={{ width: "100%", height: "100vh"  }}>
-      <YMaps>
-        <Map
-          defaultState={{ center: [ 42.872604,74.597629 ], zoom: 15 }}
-         
-          width="90%"
-          height="90%"
-          
-        >
-          <Placemark geometry={[42.872604,74.597629 ]}
-           />
-          
-        </Map>
-      </YMaps>
       
+        {/* <MapWrapper /> */}
+        <div style={{ width: "100%", height: "100vh", zIndex: 1 }}>
+        <a href="https://2gis.kg/bishkek/inside/15763234351097281/firm/70000001047984986?m=74.597365%2C42.87277%2F17.86">
+        <div style={{ position: "relative", width: "100%", height: "50vh" }}>
+  <YMaps>
+    <Map
+      defaultState={{ center: [42.872604, 74.597629], zoom: 15 }}
+      width="100%"
+      height="60%"
+      options={{
+        suppressMapOpenBlock: true,
+      }}
+    >
+      <ZoomControl options={{ float: "left" }} />
+      <Placemark geometry={[42.872604, 74.597629]} />
+    </Map>
+  </YMaps>
+
+
+  <div
+    style={{
+      position: "absolute",
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+      background: "transparent",
+      zIndex: 10,
+    }}
+  />
+</div>
+</a>
+      </div>  
+          
     </div>
       </div>
       
      
-    </div>
+  
     
   );
 };
